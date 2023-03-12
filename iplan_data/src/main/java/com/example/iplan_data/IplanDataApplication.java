91165aa51ced9ade9ea9ca0b5d7640f9d9efb702
beadinosaur <1148536889@qq.com>
2023-03-12 22:49:00 +0800

    create project

---
+++ iplan_data/src/main/java/com/example/iplan_data/IplanDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.iplan_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplanDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IplanDataApplication.class, args);
+    }
+
+}
---
+++ iplan_data/src/test/java/com/example/iplan_data/IplanDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iplan_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplanDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
